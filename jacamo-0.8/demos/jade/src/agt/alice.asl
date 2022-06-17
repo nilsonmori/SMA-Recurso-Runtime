0fa784f3a9eb3677d7a29f55ec06ffc1cecbc45d
Prof Nilson Mori <nilsonmori@yahoo.com>
2022-06-17 12:55:15 -0300

    jacamo

---
+++ jacamo-0.8/demos/jade/src/agt/alice.asl
@@ -0,0 +1,3 @@
++hello[source(A)]
+   <- .print("Hello form ", A);
+      .send(A,tell,hello).
---
+++ jacamo-0.8/doc/api/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"jacamo.infra"},{"l":"jacamo.platform"},{"l":"jacamo.project"},{"l":"jacamo.util"}]
